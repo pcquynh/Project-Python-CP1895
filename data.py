6d05d12fd0ae1c9ae557f701a5927fb6ffeb16f2
pcquynh <pcquynh@mun.ca>
2022-04-05 09:19:21 -0230

    Add code for data file

--- data.py
+++ data.py
@@ -1,33 +1,56 @@
 import csv
+from datetime import datetime
 
 
 def read_recipes_from_file(file_name):
-    pass
+    with open(file_name, mode='r') as file:
+        reader = csv.DictReader(file)
+        list_recipes = []
+        for row in reader:
+            list_recipes.append(row)
+    return list_recipes
 
 
-def write_recipes_from_file(file_name, recipes):
-    pass
+def write_recipes_to_file(file_name, recipes):
+    with open(file_name, mode='w') as file:
+        fieldnames = ['id', 'name', 'cook-time', 'servings', 'instructions', 'ingredients', 'image']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(recipes)
 
 
 def save_recipes_to_file(file_name, recipe):
-    pass
+    with open(file_name, mode='a') as file:
+        fieldnames = ['id', 'name', 'cook_time', 'servings', 'instructions', 'ingredients', 'image']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(recipe)
 
 
 def update_recipe_from_file(file_name, recipe):
-    pass
+    recipes = read_recipes_from_file(file_name)
+    index = find_recipe_by_id(recipes, recipe['id'])
+    recipes[index] = recipe
+    write_recipes_to_file(file_name, recipes)
 
 
 def delete_recipe_from_file(file_name, recipe_id):
-    pass
+    recipes = read_recipes_from_file(file_name)
+    deleted_recipe_index = find_recipe_index_by_id(recipes, recipe_id)
+    recipes.pop(deleted_recipe_index)
+    write_recipes_to_file(file_name, recipes)
 
 
 def generate_id():
-    pass
+    return datetime.now().strftime('%Y%m%d%H%M%S')
 
 
 def find_recipe_by_id(file_name, recipe_id):
-    pass
+    recipes = read_recipes_from_file(file_name)
+    index = find_recipe_index_by_id(recipes, recipe_id)
+    return recipes[index]
 
 
 def find_recipe_index_by_id(recipes, recipe_id):
-    pass
+    for i in range(len(recipes)):
+        if recipes[i]['id'] == recipe_id:
+            return i
