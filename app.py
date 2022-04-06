9d73148c5c4e2df9fbb769e9bb95c133e3e7499a
pcquynh <pcquynh@mun.ca>
2022-04-06 13:28:05 -0230

    Add route for home page and create recipe

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from data import *
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    pass
+    recipes = read_recipes_from_file('static/recipes.csv')
+    title = "Home Page"
+    return render_template("index.html", recipes=recipes, title=title)
 
 
 @app.route('/delete/<id>', methods=['POST', 'GET'])
@@ -21,7 +24,8 @@ def save_recipe():
 
 @app.route('/create', methods=['POST', 'GET'])
 def create_recipe():
-    pass
+    title = "Add recipe"
+    return render_template("create_recipe_form.html", title=title)
 
 
 @app.route('/edit/<id>', methods=['POST', 'GET'])
@@ -32,12 +36,6 @@ def edit_recipe(id):
 @app.route('/update/<id>', methods=['POST', 'GET'])
 def update_recipe(id):
     pass
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
