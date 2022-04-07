07533394cdbb4e45f363365011f681cb8caaf803
pcquynh <pcquynh@mun.ca>
2022-04-07 16:01:38 -0230

    Add update and delete recipe

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ from werkzeug.utils import secure_filename
 from data import *
 from flask import render_template, request, redirect, flash, url_for
 
-app = Flask(__name__)
+
 UPLOAD_FOLDER = 'static/images/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
 def allowed_file(filename):
@@ -24,7 +28,9 @@ def index():
 
 @app.route('/delete/<id>', methods=['POST', 'GET'])
 def delete_recipe(id):
-    pass
+    delete_recipe_from_file('static/recipes.csv', id)
+    flash('The recipe was removed successfully!')
+    return redirect(url_for('index'))
 
 
 @app.route('/save', methods=['POST'])
@@ -78,7 +84,35 @@ def edit_recipe(id):
 
 @app.route('/update/<id>', methods=['POST', 'GET'])
 def update_recipe(id):
-    pass
+    recipe = find_recipe_by_id('static/recipes.csv', id)
+    if request.method == 'POST':
+        recipe['id'] = request.form['id']
+        recipe['name'] = request.form['name']
+        recipe['cook_time'] = request.form['cook_time']
+        recipe['servings'] = request.form['servings']
+        recipe['instructions'] = request.form['instructions']
+        recipe['ingredients'] = request.form['ingredients']
+
+        # UPLOAD IMAGE
+
+        if 'file' not in request.files or request.files['file'].filename == '':
+            recipe['image'] = recipe['image']
+            update_recipe_from_file('static/recipes.csv', recipe)
+            flash('The recipe was updated successfully')
+            return redirect(url_for('index'))
+
+        elif request.files['file'] and allowed_file(request.files['file'].filename):
+            filename = secure_filename(request.files['file'].filename)
+            request.files['file'].save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print('upload_image filename: ' + filename)
+            recipe['image'] = filename
+            update_recipe_from_file('static/recipes..csv', recipe)
+            flash('The recipe was updated successfully')
+            return redirect(url_for('index'))
+
+        else:
+            flash('Allowed image types are - png, jpg, jpeg, gif')
+            return redirect(url_for('edit_recipe', id=request.form['id']))
 
 
 if __name__ == '__main__':
